0d885a9b9f920aa9d6ec3d9ad5c1b51dbd55da9b
Rickecr <richelton14@gmail.com>
2019-03-16 23:52:26 -0300

    Melhorando perfomance da aplicação: Diminuindo quantidade de partidos listados para 20 por página

--- src/Components/Partidos/partidos.js
+++ src/Components/Partidos/partidos.js
@@ -8,16 +8,39 @@ export default class Partidos extends Component {
 
     state = {
         partidos : [],
+        page : 1
     }
 
     async componentDidMount() {
-        const response = await axios.get("https://dadosabertos.camara.leg.br/api/v2/partidos?ordem=ASC&ordenarPor=nome&pagina=1&itens=40");
+        await this.acessaApi();
+    }
+
+    acessaApi = async (page = 1) => {
+        const response = await axios.get("https://dadosabertos.camara.leg.br/api/v2/partidos?ordem=ASC&ordenarPor=nome&pagina=" + page + "&itens=20");
         const { dados } = response.data;
-        this.setState({ partidos : dados });
+        this.setState({ partidos : dados, page: page });
+    }
+
+    prevPage = () => {
+        const { page } = this.state;
+
+        if (page === 1) return;
+
+        const pageNumber = page - 1;
+        this.acessaApi(pageNumber);
+    }
+
+    nextPage = () => {
+        const { page } = this.state;
+
+        if (page === 2) return;
+
+        const pageNumber = page + 1;
+        this.acessaApi(pageNumber);
     }
 
     render() {
-        const { partidos } = this.state;
+        const { partidos, page } = this.state;
 
         return (
             <div className="partidos-lista">
@@ -25,9 +48,13 @@ export default class Partidos extends Component {
                     <article key={ partido.id } >
                         <strong>{ partido.nome } - { partido.sigla }</strong>
                         <br></br>
-                        <Link to={ "/partido/"+partido.id }>Page</Link>
+                        <Link to={ "/partido/"+partido.id }>Ver Detalhes</Link>
                     </article>
                 )) }
+                <div>
+                    <button disabled={ page === 1 } onClick={ this.prevPage } >Anterior</button>
+                    <button disabled={ page === 2 } onClick={ this.nextPage } >Próxima</button>
+                </div>
             </div>
         )
     }
