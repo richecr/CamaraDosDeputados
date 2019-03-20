eb12f22ee47302d2ce478ed1b8383349e8c91e7c
Rich Elton <richelton14@gmail.com>
2019-03-20 15:37:30 -0300

    página de Deputado: Codificando

--- src/Components/Deputados/pageDeputado.js
+++ src/Components/Deputados/pageDeputado.js
@@ -5,22 +5,43 @@ export default class Deputado extends React.Component {
 
     state = {
         deputado : {},
+        load: false
     }
 
     async componentDidMount() {
         const { id } = this.props.match.params;
         const response = await axios.get('https://dadosabertos.camara.leg.br/api/v2/deputados/'+id);
         const { dados } = response.data;
-        
-        this.setState({ deputado : dados });
+        this.setState({ deputado : dados, load: true });
     }
 
     render() {
-        const { deputado } = this.state;
+        const { deputado, load } = this.state;
 
         return (
-            <div>
-                <h1>{ deputado.nomeCivil }</h1>
+            <div className="deputado">
+                <div className="titulo">
+                    <h1>{ deputado.nomeCivil } - { deputado.cpf }</h1>
+                </div>
+
+                {load &&
+                <div className="info-deputado">   
+                    <section className="dadosInfo">
+                        <h1>Informações</h1>
+                        <h3>Nome: { deputado.ultimoStatus.nome }</h3>
+                        <h3>Sigla do Partido: {deputado.ultimoStatus.siglaPartido }</h3>
+                        <h3>UF: { deputado.ultimoStatus.siglaUf }</h3>
+                        <h3>ID Legislatura: { deputado.ultimoStatus.idLegislatura }</h3>
+                    </section>
+
+                    <section>
+                        <h1>Gabinete</h1>
+                        <h3>Nome: { deputado.ultimoStatus.gabinete.nome }</h3>
+                        <h3>Prédio: { deputado.ultimoStatus.gabinete.predio }</h3>
+                        <h3>Sala: { deputado.ultimoStatus.gabinete.predio }</h3>
+                    </section>
+                </div>
+                }
             </div>
         )
     }
