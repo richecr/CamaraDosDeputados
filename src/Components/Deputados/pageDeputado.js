eb94f1f3c0977cee423e4f2252093e3e3f2fab89
Rickecr <richelton14@gmail.com>
2019-03-24 21:28:15 -0300

    Estelização de página de deputado

--- src/Components/Deputados/pageDeputado.js
+++ src/Components/Deputados/pageDeputado.js
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 
+import './stylesPageDeputado.css'
+
 export default class Deputado extends React.Component {
 
     state = {
@@ -21,25 +23,40 @@ export default class Deputado extends React.Component {
         return (
             <div className="deputado">
                 <div className="titulo">
-                    <h1>{ deputado.nomeCivil } - { deputado.cpf }</h1>
+                    <h1>{ deputado.nomeCivil } - { deputado.ufNascimento }</h1>
                 </div>
 
                 {load &&
                 <div className="info-deputado">   
-                    <section className="dadosInfo">
+                    <section className="info-dados">
                         <h1>Informações</h1>
                         <h3>Nome: { deputado.ultimoStatus.nome }</h3>
+                        <h3>CPF: { deputado.cpf }</h3>
                         <h3>Sigla do Partido: {deputado.ultimoStatus.siglaPartido }</h3>
                         <h3>UF: { deputado.ultimoStatus.siglaUf }</h3>
                         <h3>ID Legislatura: { deputado.ultimoStatus.idLegislatura }</h3>
+                        <h3>Data de Nascimento: { deputado.dataNascimento }</h3>
+                        <h3>Município de Nascimento: { deputado.municipioNascimento }</h3>
+                        <h3>Escolaridade: { deputado.escolaridade }</h3>
                     </section>
 
-                    <section>
+                    <section className="info-gabinete">
                         <h1>Gabinete</h1>
                         <h3>Nome: { deputado.ultimoStatus.gabinete.nome }</h3>
                         <h3>Prédio: { deputado.ultimoStatus.gabinete.predio }</h3>
-                        <h3>Sala: { deputado.ultimoStatus.gabinete.predio }</h3>
+                        <h3>Sala: { deputado.ultimoStatus.gabinete.sala }</h3>
+                        <h3>Andar: { deputado.ultimoStatus.gabinete.andar }</h3>
+                        <h3>Telefone: { deputado.ultimoStatus.gabinete.telefone }</h3>
+                        <h3>E-mail: { deputado.ultimoStatus.gabinete.email }</h3>
                     </section>
+
+                    <section className="info-situacao">
+                        <h1>situação</h1>
+                        <h3>situação: { deputado.ultimoStatus.situacao }</h3>
+                        <h3>Condição Eleitoral: { deputado.ultimoStatus.condicaoEleitoral }</h3>
+                        { deputado.ultimoStatus.situacao && <h3>situação: { deputado.ultimoStatus.situacao }</h3> }
+                    </section>
+                    
                 </div>
                 }
             </div>
