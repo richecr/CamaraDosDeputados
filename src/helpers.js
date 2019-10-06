c5fdc725b11c8c4b1554df7084bd5d200ec6680b
LucasLeandro1204 <lucasleandro1204@gmail.com>
2019-10-06 18:16:42 -0300

    create useDadosAbertos hook

---
+++ src/helpers.js
@@ -0,0 +1,115 @@
+import Axios from 'axios';
+import { useState, useEffect } from 'react';
+
+export const extractParamFromUrl = (key, url) => {
+  const splitted = url.split(`${key}=`)[1];
+
+  if (splitted) {
+    return Number(splitted.charAt(0));
+  }
+};
+
+export const useDadosAbertos = (resource, paramsDefault = {}) => {
+  const [params, setParams] = useState(Object.assign({
+    itens: 20,
+    pagina: 1,
+    ordem: 'ASC',
+  }, paramsDefault)); 
+  const [data, setData] = useState([
+    //
+  ]);
+  const [firstPage, setFirstPage] = useState(0);
+  const [lastPage, setLastPage] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const setParam = (key, value) => {
+    setParams({
+      ...params,
+      [key]: value,
+    });
+  };
+
+  const setPage = (page) => {
+    /**
+     * Skip if page is greater than last page or less than first page.
+     */
+    if (page > lastPage || page < firstPage) {
+      return;
+    }
+
+    setParam('pagina', page);
+  };
+
+  const nextPage = () => {
+    setPage(params.pagina + 1);
+  };
+
+  const prevPage = () => {
+    setPage(params.pagina - 1);
+  };
+
+  const updatePagination = (links) => {
+    for (const { rel, href } of links) {
+      const page = extractParamFromUrl('pagina', href);
+
+      if (! page) {
+        continue;
+      }
+
+      switch (rel) {
+        case 'first': {
+          setFirstPage(page);
+        };
+
+        case 'last': {
+          setLastPage(page);
+        };
+      };
+    }
+  };
+
+  useEffect(() => {
+    const fetch = async () => {
+      setLoading(true);
+
+      try {
+        const {
+          data: {
+            dados,
+            links,
+          },
+        } = await Axios.get(`https://dadosabertos.camara.leg.br/api/v2/${resource}`, {
+          params,
+        });
+
+        setData(dados);
+
+        if (! Array.isArray(links)) {
+          return;
+        }
+
+        updatePagination(links);
+      } catch (e) {
+        // do something?
+      }
+
+      setLoading(false);
+    };
+
+    fetch();
+  }, [
+    params,
+  ]);
+
+  return {
+    data,
+    params,
+    loading,
+    setPage,
+    setParam,
+    nextPage,
+    prevPage,
+    lastPage,
+    firstPage,
+  };
+};
