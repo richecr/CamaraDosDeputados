6ef6b86d16e710f3863cfbc191634c4e1266ed19
LucasLeandro1204 <lucasleandro1204@gmail.com>
2019-10-06 21:05:23 -0300

    import useState

--- src/Components/Partidos/partidos.js
+++ src/Components/Partidos/partidos.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import '../Partidos/partidos.css';
+import React, { useState }from 'react';
 import { Link } from 'react-router-dom';
 import { useDadosAbertos } from '../../helpers';
 
