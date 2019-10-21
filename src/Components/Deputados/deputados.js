9546d55e657091d97f779de4b6bc9d74ad8571fb
Cristiano <cristiano.goncalves@cubos.io>
2019-10-21 18:39:36 -0300

    feature: estilização da tela de deputado.

--- src/Components/Deputados/deputados.js
+++ src/Components/Deputados/deputados.js
@@ -1,109 +1,139 @@
-import './deputados.css';
+import "./deputados.css";
 /* eslint-disable jsx-a11y/alt-text */
-import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
-import { useDadosAbertos } from '../../helpers';
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useDadosAbertos } from "../../helpers";
 
 const Deputados = () => {
-    const [name, setName] = useState('');
-    const {
-        params,
-        prevPage,
-        setParam,
-        lastPage,
-        nextPage,
-        firstPage,
-        data: deputados,
-    } = useDadosAbertos('deputados', {
-        nome: name,
-        ordenarPor: 'nome',
-    },
-    );
+	const [name, setName] = useState("");
+	const {
+		params,
+		prevPage,
+		setParam,
+		lastPage,
+		nextPage,
+		firstPage,
+		data: deputados
+	} = useDadosAbertos("deputados", {
+		nome: name,
+		ordenarPor: "nome"
+	});
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+	const handleSubmit = event => {
+		event.preventDefault();
 
-        setParam('nome', name);
-    };
+		setParam("nome", name);
+	};
 
-    return (
-        <div className="columns columns-fix">
-            <nav className="panel column is-3">
-                <p className="panel-heading">Deputados</p>
-                <div className="panel-block">
-                    <p className="control has-icons-left">
-                        <form className="search" onSubmit={handleSubmit}>
-                            <div className="field has-addons">
-                                <div className="control">
-                                    <input
-                                        className="input"
-                                        placeholder="Buscar por nome"
-                                        value={name}
-                                        onChange={e => setName(e.target.value)}
-                                    />
-                                </div>
-                                <div className="control">
-                                    <button className="button is-info">Buscar</button>
-                                </div>
-                            </div>
-                        </form>
-                    </p>
-                </div>
-                <Link className="panel-block" onClick={() => setParam('ordenarPor', 'id')}>
-                    ID
-            </Link>
-                <Link className="panel-block" onClick={() => setParam('ordenarPor', 'idLegislatura')}>
-                    Id da Legislatura
-            </Link>
-                <Link className="panel-block" onClick={() => setParam('ordenarPor', 'nome')}>
-                    Nome
-            </Link>
-                <Link className="panel-block" onClick={() => setParam('ordenarPor', 'siglaUF')}>
-                    Sigla de UF
-            </Link>
-                <Link className="panel-block" onClick={() => setParam('ordenarPor', 'siglaPartido')}>
-                    Sigla do Partido
-            </Link>
-            </nav>
+	return (
+		<div className="columns columns-fix">
+			<nav className="panel column is-3">
+				<p className="panel-heading">Deputados</p>
+				<div className="panel-block">
+					<p className="control has-icons-left">
+						<form className="search" onSubmit={handleSubmit}>
+							<div className="field has-addons">
+								<div className="control">
+									<input
+										className="input"
+										placeholder="Buscar por nome"
+										value={name}
+										onChange={e => setName(e.target.value)}
+									/>
+								</div>
+								<div className="control">
+									<button className="button is-info">Buscar</button>
+								</div>
+							</div>
+						</form>
+					</p>
+				</div>
+				<Link
+					className="panel-block"
+					onClick={() => setParam("ordenarPor", "id")}
+				>
+					ID
+				</Link>
+				<Link
+					className="panel-block"
+					onClick={() => setParam("ordenarPor", "idLegislatura")}
+				>
+					Id da Legislatura
+				</Link>
+				<Link
+					className="panel-block"
+					onClick={() => setParam("ordenarPor", "nome")}
+				>
+					Nome
+				</Link>
+				<Link
+					className="panel-block"
+					onClick={() => setParam("ordenarPor", "siglaUF")}
+				>
+					Sigla de UF
+				</Link>
+				<Link
+					className="panel-block"
+					onClick={() => setParam("ordenarPor", "siglaPartido")}
+				>
+					Sigla do Partido
+				</Link>
+			</nav>
 
-            <div className="partidos-lista columns-fix">
-                <div className="columns">
-                    <div className="columns is-multiline is-mobile">
-                        {deputados.map(deputado => (
-                            <div className="column deputados-lista is-two-fifths">
-                                <div class="card" key={deputado.id}>
-                                    <div class="card-image">
-                                        <figure class="image is-4by3">
-                                            <img width="114" height="152" src={deputado.urlFoto}></img>
-                                        </figure>
-                                    </div>
-                                    <div class="card-content">
-                                        <div class="media">
-                                            <div class="media-content">
-                                                <p class="title is-4">{deputado.nome}</p>
-                                                <p class="subtitle is-6">Partido: {deputado.siglaPartido}</p>
-                                            </div>
-                                        </div>
+			<div className="partidos-lista columns-fix">
+				<div className="columns">
+					<div className="columns is-multiline is-mobile">
+						{deputados.map(deputado => (
+							<div className="column deputados-lista is-two-fifths">
+								<div class="card" key={deputado.id}>
+									<div class="card-image">
+										<figure class="image is-4by3">
+											<img
+												width="114"
+												height="152"
+												className="deputado-img"
+												src={deputado.urlFoto}
+											/>
+										</figure>
+									</div>
+									<div class="card-content">
+										<div class="media">
+											<div class="media-content deputado-info">
+												<p class="title is-4">{deputado.nome}</p>
+												<p class="tags has-addons">
+													<span class="tag">Partido:</span>
+													<span class="tag is-primary">
+														{deputado.siglaPartido}
+													</span>
+												</p>
+											</div>
+										</div>
 
-                                        <div class="content">
-                                            <Link className="detalhes-link card-footer-item" to={"/deputado/" + deputado.id}>Ver Detalhes</Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <div className="deputados-buttons">
-                    <button disabled={params.pagina <= firstPage} onClick={prevPage} >Anterior</button>
-                    <button disabled={params.pagina >= lastPage} onClick={nextPage} >Próxima</button>
-                </div>
-            </div>
-        </div>
-
-
-
-    );
-}
+										<div class="content">
+											<Link
+												className="detalhes-link card-footer-item"
+												to={"/deputado/" + deputado.id}
+											>
+												Ver Detalhes
+											</Link>
+										</div>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+				<div className="deputados-buttons">
+					<button disabled={params.pagina <= firstPage} onClick={prevPage}>
+						Anterior
+					</button>
+					<button disabled={params.pagina >= lastPage} onClick={nextPage}>
+						Próxima
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Deputados;
--- src/Components/Deputados/pageDeputado.js
+++ src/Components/Deputados/pageDeputado.js
@@ -1,65 +1,140 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import './stylesPageDeputado.css'
+import "./stylesPageDeputado.css";
 
-const Deputado = ({
-  match,
-}) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [deputado, setDeputado] = useState(null);
+const Deputado = ({ match }) => {
+	const [isLoading, setIsLoading] = useState(true);
+	const [deputado, setDeputado] = useState(null);
 
-  useEffect(() => {
-    async function fetchDeputado() {
-      const { id } = match.params;
-      const { data } = await axios.get(`https://dadosabertos.camara.leg.br/api/v2/deputados/${id}`);
-  
-      setIsLoading(false);
-      setDeputado(data.dados);
-    }
+	useEffect(() => {
+		async function fetchDeputado() {
+			const { id } = match.params;
+			const { data } = await axios.get(
+				`https://dadosabertos.camara.leg.br/api/v2/deputados/${id}`
+			);
 
-    fetchDeputado();
-  }, [match]);
+			setIsLoading(false);
+			setDeputado(data.dados);
+		}
 
-  return (
-    <div className="deputado">
-      {!isLoading && deputado !== null &&
-        <>
-          <div className="titulo">
-            <h1>{ deputado.nomeCivil } - { deputado.ufNascimento }</h1>
-          </div>
-          <div className="info-deputado">   
-            <section className="info-dados">
-              <h1>Informações</h1>
-              <h3>Nome: { deputado.ultimoStatus.nome }</h3>
-              <h3>CPF: { deputado.cpf }</h3>
-              <h3>Sigla do Partido: {deputado.ultimoStatus.siglaPartido }</h3>
-              <h3>UF: { deputado.ultimoStatus.siglaUf }</h3>
-              <h3>ID Legislatura: { deputado.ultimoStatus.idLegislatura }</h3>
-              <h3>Data de Nascimento: { deputado.dataNascimento }</h3>
-              <h3>Município de Nascimento: { deputado.municipioNascimento }</h3>
-              <h3>Escolaridade: { deputado.escolaridade }</h3>
-            </section>
-            <section className="info-gabinete">
-              <h1>Gabinete</h1>
-              <h3>Nome: { deputado.ultimoStatus.gabinete.nome }</h3>
-              <h3>Prédio: { deputado.ultimoStatus.gabinete.predio }</h3>
-              <h3>Sala: { deputado.ultimoStatus.gabinete.sala }</h3>
-              <h3>Andar: { deputado.ultimoStatus.gabinete.andar }</h3>
-              <h3>Telefone: { deputado.ultimoStatus.gabinete.telefone }</h3>
-              <h3>E-mail: { deputado.ultimoStatus.gabinete.email }</h3>
-            </section>
-            <section className="info-situacao">
-              <h1>situação</h1>
-              <h3>situação: { deputado.ultimoStatus.situacao }</h3>
-              <h3>Condição Eleitoral: { deputado.ultimoStatus.condicaoEleitoral }</h3>
-              { deputado.ultimoStatus.situacao && <h3>situação: { deputado.ultimoStatus.situacao }</h3> }
-            </section>
-          </div>
-        </>
-      }
-    </div>
-  );
+		fetchDeputado();
+	}, [match]);
+
+	return (
+		<div className="deputado">
+			{!isLoading && deputado !== null && (
+				<>
+					<div className="title">
+						<figure class="deputado-photo">
+							<img
+								width="114"
+								height="152"
+								alt={deputado.nomeCivil}
+								src={deputado.ultimoStatus.urlFoto}
+							/>
+						</figure>
+						<h1>
+							{deputado.nomeCivil} - {deputado.ufNascimento}
+						</h1>
+						<div class="basic-info">
+							<h3 class="info-text tags has-addons">
+								<span class="tag is-info ">Condição Eleitoral: </span>
+								<span class="tag is-white">
+									{deputado.ultimoStatus.condicaoEleitoral}
+								</span>
+							</h3>
+							{deputado.ultimoStatus.situacao && (
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-info ">Situação: </span>
+									<span class="tag is-white">
+										{deputado.ultimoStatus.situacao}
+									</span>
+								</h3>
+							)}
+						</div>
+					</div>
+					<div className="info-deputado">
+						<section className="info-card">
+							<h2 class=" title is-4">Informações</h2>
+							<div class="info-card-container">
+								<h3 class="tags has-addons info-text">
+									<span class="tag is-primary ">Nome:</span>
+									<span class="tag">{deputado.ultimoStatus.nome}</span>
+								</h3>
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-primary ">CPF: </span>
+									<span class="tag">{deputado.cpf}</span>
+								</h3>
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-primary ">Sigla do Partido: </span>
+									<span class="tag">{deputado.ultimoStatus.siglaPartido}</span>
+								</h3>
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-primary ">UF: </span>
+									<span class="tag">{deputado.ultimoStatus.siglaUf}</span>
+								</h3>
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-primary ">ID Legislatura: </span>
+									<span class="tag">{deputado.ultimoStatus.idLegislatura}</span>
+								</h3>
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-primary ">Data de Nascimento: </span>
+									<span class="tag">{deputado.dataNascimento}</span>
+								</h3>
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-primary ">Município de Nascimento: </span>
+									<span class="tag">{deputado.municipioNascimento}</span>
+								</h3>
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-primary ">Escolaridade: </span>
+									<span class="tag">{deputado.escolaridade}</span>
+								</h3>
+							</div>
+						</section>
+
+						<section className="info-card">
+							<h2 class="title is-4">Gabinete</h2>
+							<div class="info-card-container">
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-primary ">Nome: </span>
+									<span class="tag">{deputado.ultimoStatus.gabinete.nome}</span>
+								</h3>
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-primary ">Prédio: </span>
+									<span class="tag">
+										{deputado.ultimoStatus.gabinete.predio}
+									</span>
+								</h3>
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-primary ">Sala: </span>
+									<span class="tag">{deputado.ultimoStatus.gabinete.sala}</span>
+								</h3>
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-primary ">Andar: </span>
+									<span class="tag">
+										{deputado.ultimoStatus.gabinete.andar}
+									</span>
+								</h3>
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-primary ">Telefone: </span>
+									<span class="tag">
+										{deputado.ultimoStatus.gabinete.telefone}
+									</span>
+								</h3>
+								<h3 class="info-text tags has-addons">
+									<span class="tag is-primary ">E-mail: </span>
+									<span class="tag">
+										{deputado.ultimoStatus.gabinete.email}
+									</span>
+								</h3>
+							</div>
+						</section>
+					</div>
+				</>
+			)}
+		</div>
+	);
 };
 
 export default Deputado;
