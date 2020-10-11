459d037ca61fdc1730a18a2038f23636c52699c0
rickecr <richelton14@gmail.com>
2020-10-11 20:00:36 -0300

    correcoes finais

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Header from './pages/Header';
+import Header from './components/Header';
 import Deputados from './pages/Deputados';
 import Partidos from './pages/Partidos';
 
