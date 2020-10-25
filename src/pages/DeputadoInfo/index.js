8dd4ec84030ec6d5d60eacc427960c1c2a9c1365
mathsantana <matheus.santana@ccc.ufcg.edu.br>
2020-10-25 16:46:42 -0300

    card informacao Deputado

--- src/pages/DeputadoInfo/index.js
+++ src/pages/DeputadoInfo/index.js
@@ -20,6 +20,7 @@ const DeputadoInfo = ({ match }) => {
       );
 
       setIsLoading(false);
+      console.log(data.dados);
       setDeputado(data.dados);
     }
 
@@ -31,116 +32,108 @@ const DeputadoInfo = ({ match }) => {
       {!isLoading && deputado !== null && (
         <>
           <div className="title">
-            <figure className="deputado-photo">
-              <img
-                width="114"
-                height="152"
-                alt={deputado.nomeCivil}
-                src={deputado.ultimoStatus.urlFoto}
-                onError={(e) => {
-                  e.currentTarget.src = noImage;
-                }}
-              />
-            </figure>
-            <h1>
-              {deputado.nomeCivil} - {deputado.ufNascimento}
-            </h1>
-            <div className="basic-info">
-              <Tag
-                label="Condição Eleitoral: "
-                value={deputado.ultimoStatus.condicaoEleitoral}
-                white
-                info
-              />
-
-              {deputado.ultimoStatus.situacao && (
-                <Tag
-                  label="Situação: "
-                  value={deputado.ultimoStatus.situacao}
-                  white
-                  info
-                />
-              )}
-            </div>
+            <div className="basic-info"></div>
           </div>
           <div className="info-deputado">
+            <section className="avatar"></section>
             <section className="info-card">
-              <h2 className=" title is-4">Informações</h2>
               <div className="info-card-container">
-                <Tag
-                  label="Nome: "
-                  value={deputado.ultimoStatus.nome}
-                  primary
-                />
-                <Tag label="CPF: " value={deputado.ultimoStatus.cpf} primary />
-                <Tag
-                  label="Sigla do Partido: "
-                  value={deputado.ultimoStatus.siglaPartido}
-                  primary
-                />
-                <Tag
-                  label="UF: "
-                  value={deputado.ultimoStatus.siglaUf}
-                  primary
-                />
-                <Tag
-                  label="ID Legislatura: "
-                  value={deputado.ultimoStatus.idLegislatura}
-                  primary
-                />
-                <Tag
-                  label="Data de Nascimento: "
-                  value={deputado.ultimoStatus.dataNascimento}
-                  primary
-                />
-                <Tag
-                  label=" Município de Nascimento: "
-                  value={deputado.ultimoStatus.municipioNascimento}
-                  primary
-                />
-                <Tag
-                  label="Escolaridade: "
-                  value={deputado.ultimoStatus.escolaridade}
-                  primary
-                />
+                <div className="info-card-avatar">
+                  <figure className="deputado-photo">
+                    <img
+                      width="114"
+                      height="152"
+                      id="avatar"
+                      alt={deputado.nomeCivil}
+                      src={deputado.ultimoStatus.urlFoto}
+                      onError={(e) => {
+                        e.currentTarget.src = noImage;
+                      }}
+                    />
+                  </figure>
+
+                  <Tag
+                    label="Condição Eleitoral: "
+                    value={deputado.ultimoStatus.condicaoEleitoral}
+                    white
+                    info
+                  />
+
+                  {deputado.ultimoStatus.situacao && (
+                    <Tag
+                      label="Situação: "
+                      value={deputado.ultimoStatus.situacao}
+                      white
+                      info
+                    />
+                  )}
+                </div>
+                <div className="info-card-data">
+                  <div className="info-card-data-personal">
+                    <div className="info-card-data-header">
+                      <h2 className="title is-4">{deputado.nomeCivil}</h2>
+                    </div>
+                    <div className="info-card-data-section bordered">
+                      <div className="info-card-data-subsection">
+                        <p className="info">
+                          Estado: {deputado.ultimoStatus.siglaUf}
+                        </p>
+                        <p className="info">
+                          Nome: {deputado.ultimoStatus.nome}
+                        </p>
+                        <p className="info">CPF: {deputado.cpf}</p>
+                        <p className="info">
+                          Sigla Partido: {deputado.ultimoStatus.siglaPartido}
+                        </p>
+                      </div>
+                      <div className="info-card-data-subsection">
+                        <p className="info">
+                          ID Legislatura: {deputado.ultimoStatus.idLegislatura}
+                        </p>
+                        <p className="info">
+                          Data de Nascimento: {deputado.dataNascimento}
+                        </p>
+                        <p className="info">
+                          Naturalidade: {deputado.municipioNascimento}
+                        </p>
+                        <p className="info">
+                          Nível de escolaridade: {deputado.escolaridade}
+                        </p>
+                      </div>
+                    </div>
+                    <hr className="divider" />
+                    <div className="info-card-data-header">
+                      <h3 className="title is-4">Gabinete</h3>
+                    </div>
+                    <div className="info-card-data-section">
+                      <div className="info-card-data-subsection">
+                        <p className="info">
+                          Nome: {deputado.ultimoStatus.gabinete.nome}
+                        </p>
+                        <p className="info">
+                          Prédio: {deputado.ultimoStatus.gabinete.predio}
+                        </p>
+                        <p className="info">
+                          Sala: {deputado.ultimoStatus.gabinete.sala}
+                        </p>
+                      </div>
+                      <div className="info-card-data-subsection">
+                        <p className="info">
+                          Andar: {deputado.ultimoStatus.gabinete.andar}
+                        </p>
+                        <p className="info">
+                          Telefone: {deputado.ultimoStatus.gabinete.telefone}
+                        </p>
+                        <p className="info">
+                          Email: {deputado.ultimoStatus.gabinete.email}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </section>
 
-            <section className="info-card">
-              <h2 className="title is-4">Gabinete</h2>
-              <div className="info-card-container">
-                <Tag
-                  label="Nome: "
-                  value={deputado.ultimoStatus.gabinete.nome}
-                  primary
-                />
-                <Tag
-                  label="Prédio: "
-                  value={deputado.ultimoStatus.gabinete.predio}
-                  primary
-                />
-                <Tag
-                  label="Sala: "
-                  value={deputado.ultimoStatus.gabinete.sala}
-                  primary
-                />
-                <Tag
-                  label="Andar: "
-                  value={deputado.ultimoStatus.gabinete.andar}
-                  primary
-                />
-                <Tag
-                  label="Telefone: "
-                  value={deputado.ultimoStatus.gabinete.telefone}
-                  primary
-                />
-                <Tag
-                  label="E-mail: "
-                  value={deputado.ultimoStatus.gabinete.email}
-                  primary
-                />
-              </div>
-            </section>
             <Despesas id={id} />
           </div>
         </>
