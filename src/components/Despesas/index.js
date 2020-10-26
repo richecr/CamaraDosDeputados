3c16c939a0b2755bdf33a37a235b121da5cacd7a
mathsantana <matheus.santana@ccc.ufcg.edu.br>
2020-10-26 01:33:29 -0300

    deputadoInfo DONE

--- src/components/Despesas/index.js
+++ src/components/Despesas/index.js
@@ -1,25 +1,54 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import { useDadosAbertos } from '../../helpers';
+import Axios from 'axios';
+import {extractParamFromUrl} from '../../helpers'
 import Paginacao from '../Pagination';
 import Loader from '../Loader';
-import Tag from '../Tag';
+import { MdAttachMoney } from 'react-icons/md';
 
 import './styles.scss';
 
 const Despesas = ({ id }) => {
-  const {
-    params,
-    prevPage,
-    lastPage,
-    nextPage,
-    firstPage,
-    loading,
-    data: despesas,
-  } = useDadosAbertos(`/deputados/${id}/despesas`, {
-    ordem: 'DESC',
-    ordenarPor: 'dataDocumento',
-  });
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [despesas, setDespesas] = useState([]);
+
+
+  useEffect(() => {
+    (async () => getDespesas())();
+  }, [currentPage])
+
+  const getDespesas = async () => {
+      setLoading(true);
+      try {
+        const {
+          data: {dados, links}
+        } = await Axios.get(
+          `https://dadosabertos.camara.leg.br/api/v2/deputados/${id}/despesas`,
+          {
+            params: {
+
+          ordem: 'DESC',
+          ordenarPor: 'dataDocumento',
+          itens: 10,
+          pagina: currentPage,
+            }
+          }
+        );
+
+        const lastUrl = links.find((link) => link.rel === 'last');
+
+        setTotalPages(extractParamFromUrl('pagina', lastUrl.href))
+        setDespesas(dados);
+
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+      };
+  
   const ref = useRef(null);
 
   if (loading) return <Loader />;
@@ -27,9 +56,16 @@ const Despesas = ({ id }) => {
   if (ref.current) {
     window.scrollTo(0, ref.current.offsetTop);
   }
+  
+  const onPageChange = (data) => {
+    const selectedPage = data.selected + 1;
+    setCurrentPage(selectedPage);
+  }
+
+
 
   return (
-    <section className="info-card" ref={ref}>
+    <div className="despesas">
       <h2 className="title is-4">Despesas</h2>
       {despesas.map((despesa) => {
         return (
@@ -37,32 +73,44 @@ const Despesas = ({ id }) => {
             className="box despesa"
             key={despesa.codDocumento || despesa.numDocumento}
           >
-            <Tag label="Fornecedor: " value={despesa.nomeFornecedor} primary />
-            <Tag
-              label="Tipo de despesa: "
-              value={despesa.tipoDespesa}
-              primary
-            />
-            <Tag label="Data: " value={despesa.dataDocumento} primary />
-            <Tag label="Valor: " value={despesa.valorDocumento} primary />
+            <div className="despesa-section">
+              <div className="icon">
+                <MdAttachMoney />
+              </div>
+            </div>
+            <div className="despesa-section">
+              <p className="description">Tipo de despesa:</p>
+              <p className="content">{despesa.tipoDespesa}</p>
+            </div>
+            <div className="despesa-section">
+              <p className="description">Valor:</p>
+              <p className="content">{despesa.valorDocumento}</p>
+            </div>
+            <div className="despesa-section">
+              <p className="description">Data:</p>
+              <p className="content">{despesa.dataDocumento}</p>
+            </div>
+            <div className="despesa-section">
+              <p className="description">Fornecedor:</p>
+              <p className="content">{despesa.nomeFornecedor}</p>
+            </div>
           </div>
         );
       })}
       <div>
         <Paginacao
-          prevPage={prevPage}
-          nextPage={nextPage}
-          pagina={params.pagina}
-          firstPage={firstPage}
-          lastPage={lastPage}
+          totalPages={totalPages}
+          onPageChange={onPageChange}
+          forcePage={currentPage - 1}
         />
       </div>
-    </section>
+
+    </div>
   );
 };
 
 Despesas.propTypes = {
-  id: PropTypes.number.isRequired,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
 };
 
 export default Despesas;
