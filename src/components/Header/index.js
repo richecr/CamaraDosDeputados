9312838890c46fd3afeee10554dee0d988efe9d0
mathsantana <matheus.santana@ccc.ufcg.edu.br>
2020-10-26 21:40:14 -0300

    adding the new logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaGithub, FaSearch } from 'react-icons/fa';
 
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/logo.png';
 import './styles.scss';
 
 export default function Header() {
   return (
     <header className="header">
       <div className="headerLeft">
-        <Link to="/">
-          <img src={logo} alt="Logo da Aplicação" />
-        </Link>
+        <div id="logo-container">
+          <Link to="/">
+            <img
+              src={logo}
+              height="auto"
+              width="100%"
+              alt="Logo da Aplicação"
+            />
+          </Link>
+        </div>
         <ul>
           <li>
             <Link to="/deputados/">Deputados</Link>
